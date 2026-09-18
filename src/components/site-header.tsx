d1a5e2132430b3737d513326052531e618893872
tharindurasmal <tharindurasmal1@gmail.com>
2026-09-18 11:14:29 +0530

    cache rename logo

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -21,7 +21,7 @@ export async function SiteHeader() {
       <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
       <Link href="/" className="flex items-center gap-3 text-xl font-black tracking-tight text-slate-900">
         <span className="relative flex h-30 w-28 items-center justify-center">
-          <Image src="/logo.png" alt="AutoLanka" fill priority />
+          <Image src="/newlogo.png" alt="AutoLanka" fill priority />
         </span>
       </Link>
 
